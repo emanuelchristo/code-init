34261ae5f9669d193cbd06e34e584e52ec8c9c5d
emanuelchristo <emanuelchristochris@gmail.com>
2021-12-27 13:17:11 +0530

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Footer from 'components/Footer'
 import Navbar from 'components/Navbar'
+import Head from 'next/head'
 
 import 'styles/globals.css'
 import 'tailwindcss/tailwind.css'
@@ -7,6 +8,13 @@ import 'tailwindcss/tailwind.css'
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Head>
+				<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+				<meta name='msapplication-TileColor' content='#212121' />
+				<meta name='theme-color' content='#ffffff' />
+			</Head>
 			<Navbar />
 			<Component {...pageProps} />
 			<Footer />
